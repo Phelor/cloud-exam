0a0ade51afd887a1ad7f4dda2adcb2bfa73ee8e9
Baptiste R <bapt.rousselot@gmail.com>
2025-06-23 21:05:33 +0200

    référencement des fichiers static

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,23 @@ resource "google_storage_bucket" "cloudexam" {
   location = var.gcp_region
 }
 
+resource "google_storage_bucket_object" "site_files" {
+  for_each = fileset("${path.module}/site", "*/")
+
+  name   = each.value
+  source = "${path.module}/site/${each.value}"
+  bucket = google_storage_bucket.static_site.name
+  content_type = lookup({
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    gif  = "image/gif"
+  }, lower(regex("\\.([a-z0-9]+)$", each.value)[0]), "application/octet-stream")
+}
+
 resource "google_storage_bucket_object" "sitesrc" {
   name = "index.html"
   source = "../site/index.html"
