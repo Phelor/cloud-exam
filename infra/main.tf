5315ebe2955fc3c25f276db2dc478ee704ba6cc1
Baptiste R <bapt.rousselot@gmail.com>
2025-06-23 20:46:05 +0200

    variables et fichier static

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,21 @@ resource "google_storage_bucket" "cloudexam" {
   name = var.bucket_name
   location = "UE"
 }
+
+resource "google_storage_bucket_object" "sitesrc" {
+  name = "index.html"
+  source = "../site/index.html"
+  bucket = google_storage_bucket.cloudexam.name
+}
+
+resource "google_storage_bucket_object" "sitesrc" {
+  name = "script.js"
+  source = "../site/script.js"
+  bucket = google_storage_bucket.cloudexam.name
+}
+
+resource "google_storage_bucket_object" "sitesrc" {
+  name = "styles.css"
+  source = "../site/styles.css"
+  bucket = google_storage_bucket.cloudexam.name
+}
